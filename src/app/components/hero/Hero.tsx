79b389bbaa2b34d890e39be8b167731d5e75fa9e
satrop <stevenportas@me.com>
2025-02-02 11:44:56 -0800

    Re-run the build as there was a linter error

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { motion, useScroll, useTransform } from "framer-motion";``
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import Button from "../button/button";
 
