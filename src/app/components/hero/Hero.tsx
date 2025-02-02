6855c9fa50a2dd8408a9b5cc09ff5ef51daa6bfb
satrop <stevenportas@me.com>
2025-02-02 11:38:34 -0800

    Running build

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";``
 
 import Button from "../button/button";
